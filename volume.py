085f59561373d6bf70d0123459756c74a163022a
Ismail Sunni <imajimatika@gmail.com>
2020-04-11 11:01:03 +0200

    Finish volume script.

--- volume.py
+++ volume.py
@@ -16,19 +16,23 @@ def scrap_volume(volume):
     html_page = r.data
     soup = BeautifulSoup(html_page, 'html.parser')
     volume_section = soup.findAll("section", {"class": "pi-item pi-group pi-border-color"})[0] 
-    items = ['chapter', 'ename', 'page', ]
+    items = ['chapters', 'ename', 'page']
     # Chapter
-    chapter = volume_section.findAll("div", {"data-source": "chapters"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
-    volume_info['chapter'] = chapter
+    # chapter = volume_section.findAll("div", {"data-source": "chapters"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
+    # volume_info['chapter'] = chapter
     # Chapter
-    ename = volume_section.findAll("div", {"data-source": "ename"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
-    volume_info['ename'] = ename
-    return html_page
-
-# last_chapter = 10
-# for i in range(1, last_chapter):
-#     chapter_url = base_url + str(i)
-#     print(chapter_url)
+    # ename = volume_section.findAll("div", {"data-source": "ename"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
+    # volume_info['ename'] = ename
+    for item in items:
+        try:
+            volume_info[item] = volume_section.findAll("div", {"data-source": item})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
+        except IndexError as e:
+            print(item, e)
+    return volume_info
 
 if __name__ == "__main__":
-    scrap_volume(1)
+    last_chapter = 10
+    for chapter in range(1, last_chapter):
+        # chapter_url = base_url + str(i)
+        # print(chapter_url)
+        print(scrap_volume(chapter))
