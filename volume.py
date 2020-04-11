f7b572855e332727b5444afa74e1f7f6567b7969
Ismail Sunni <imajimatika@gmail.com>
2020-04-11 11:13:31 +0200

    Add chapter info scrap.

--- volume.py
+++ volume.py
@@ -6,9 +6,7 @@ base_url = 'https://onepiece.fandom.com/wiki/Volume_'
 
 def scrap_volume(volume):
     """Scrap volume."""
-    volume_info = {
-
-    }
+    volume_info = {}
     volume_url = base_url + str(volume)
     print(volume_url)
     http_pool = urllib3.PoolManager()
@@ -17,12 +15,6 @@ def scrap_volume(volume):
     soup = BeautifulSoup(html_page, 'html.parser')
     volume_section = soup.findAll("section", {"class": "pi-item pi-group pi-border-color"})[0] 
     items = ['chapters', 'ename', 'page']
-    # Chapter
-    # chapter = volume_section.findAll("div", {"data-source": "chapters"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
-    # volume_info['chapter'] = chapter
-    # Chapter
-    # ename = volume_section.findAll("div", {"data-source": "ename"})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
-    # volume_info['ename'] = ename
     for item in items:
         try:
             volume_info[item] = volume_section.findAll("div", {"data-source": item})[0].findAll('div', {'class': 'pi-data-value'})[0].text 
@@ -31,8 +23,6 @@ def scrap_volume(volume):
     return volume_info
 
 if __name__ == "__main__":
-    last_chapter = 10
-    for chapter in range(1, last_chapter):
-        # chapter_url = base_url + str(i)
-        # print(chapter_url)
-        print(scrap_volume(chapter))
+    last_volume = 10
+    for volume in range(1, last_volume + 1):
+        print(scrap_volume(volume))
