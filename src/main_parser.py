a30ae25fce9768e08b357c0e69a1375a02036541
Ismail Sunni <imajimatika@gmail.com>
2024-12-07 20:34:28 +0700

    Update parser to get each table timing.

--- src/main_parser.py
+++ src/main_parser.py
@@ -3,24 +3,22 @@ from parser.db_creation import (
     create_tables,
     load_volume,
     load_chapters,
+    load_coc,
     load_characters,
 )
 
 
 if __name__ == "__main__":
     db_path = "./data/op.duckdb"
-
-    conn = create_db(db_path)
-
-    create_tables(conn)
-
     volumes_json_path = "./data/volumes.json"
 
+    conn = create_db(db_path)
+    create_tables(conn)
+
     load_volume(conn, volumes_json_path)
-
     load_chapters(conn, "./data/chapters.csv")
-
     load_characters(conn, "./data/characters.json")
+    load_coc(conn, "./data/coc.csv")
 
     conn.close()
 
