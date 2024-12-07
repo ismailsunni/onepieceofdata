91174eb203377d021ead4047a7119e44a4fd2eb3
Ismail Sunni <imajimatika@gmail.com>
2024-12-07 11:46:26 +0700

    Export to db for volume and chapter.

---
+++ src/parser/db_creation.py
@@ -0,0 +1,99 @@
+import duckdb
+import json
+import pandas as pd
+
+
+def create_db(duckdb_path: str) -> duckdb.DuckDBPyConnection:
+    return duckdb.connect(duckdb_path)
+
+
+def create_tables(conn: duckdb.DuckDBPyConnection):
+    conn.execute("DROP TABLE IF EXISTS coc CASCADE")
+    conn.execute("DROP TABLE IF EXISTS chapters CASCADE")
+    conn.execute("DROP TABLE IF EXISTS volume CASCADE")
+    conn.execute("DROP TABLE IF EXISTS characters CASCADE")
+
+    conn.execute(
+        """
+        CREATE TABLE volume (
+            volume_number INTEGER PRIMARY KEY,
+            english_title TEXT,
+        )
+        """
+    )
+
+    conn.execute(
+        """
+        CREATE TABLE chapters (
+            chapter INTEGER PRIMARY KEY,
+            volume INTEGER,
+            title TEXT,
+            pages INTEGER,
+            date DATE,
+            jump TEXT,
+            FOREIGN KEY(volume) REFERENCES volume(volume_number)
+        )
+        """
+    )
+
+    conn.execute(
+        """
+        CREATE TABLE characters (
+            id INTEGER PRIMARY KEY,
+            ename TEXT,
+            origin TEXT,
+            status TEXT,
+            birth DATE,
+            blood_type TEXT
+        )
+        """
+    )
+
+    conn.execute(
+        """
+        CREATE TABLE coc (
+            chapter INTEGER,
+            character INTEGER,
+            note TEXT,
+        FOREIGN KEY(chapter) REFERENCES chapters(chapter),
+        FOREIGN KEY(character) REFERENCES characters(id)
+        )
+        """
+    )
+
+
+def load_volume(conn: duckdb.DuckDBPyConnection, volume_json_path: str):
+    with open(volume_json_path, "r") as f:
+        volumes = json.load(f)
+    for volume in volumes:
+        volume_number = volume["volume_number"]
+        title = volume["english_title"]
+        sql = """
+            INSERT INTO volume (volume_number, english_title)
+            VALUES (?, ?)
+            """
+        # print(sql, (volume_number, title))
+        conn.execute(sql, (volume_number, title))
+
+
+def load_chapters(conn: duckdb.DuckDBPyConnection, chapters_csv_path: str):
+    chapters = pd.read_csv(chapters_csv_path)
+    for _, chapter in chapters.iterrows():
+        chapter_number = int(chapter["chapter"])
+        volume = chapter["volume"]
+        if pd.isna(volume):
+            volume = None
+        title = chapter["name"]
+        pages = int(chapter["page"])
+        date = chapter["date"]
+        jump = chapter["jump"]
+        sql = """
+            INSERT INTO chapters (chapter, volume, title, pages, date, jump)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """
+        try:
+            conn.execute(sql, (chapter_number, volume, title, pages, date, jump))
+        except Exception as e:
+            print(e)
+            print(sql, (chapter_number, volume, title, pages, date, jump))
+            continue
