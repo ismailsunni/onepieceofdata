71a257ae499af1e75d0667cdf2ffdfdbfa3178c8
Ismail Sunni <imajimatika@gmail.com>
2024-12-07 20:53:05 +0700

    Refactor main parse and scrapper. All good now.

--- src/main_scrapper.py
+++ src/main_scrapper.py
@@ -1,27 +1,20 @@
+import configs
 from scrapper.chapter import scrap_chapters
 from scrapper.to_csv import convert_chapters_to_csv
 from scrapper.volume import scrap_volumes
 from scrapper.character import scrap_characters
 
 if __name__ == "__main__":
-    # Last chapter and volume
-    last_chapter = 1132
-    last_volume = 110
-
-    # JSON files
-    chapters_json_path = "./data/chapters.json"
-    volumes_json_path = "./data/volumes.json"
-    characters_json_path = "./data/characters.json"
-
-    # CSV files
-    characters_csv = "./data/characters.csv"
-    chapters_csv = "./data/chapters.csv"
-    coc_csv = "./data/coc.csv"
-
+    print("###### Start scraping data ######")
     # Scrap all chapters and volumes
-    scrap_chapters(last_chapter, chapters_json_path)
-    convert_chapters_to_csv(chapters_json_path, characters_csv, chapters_csv, coc_csv)
-    scrap_volumes(last_volume, volumes_json_path)
-    scrap_characters(characters_csv, characters_json_path)
+    scrap_chapters(configs.last_chapter, configs.chapters_json_path)
+    convert_chapters_to_csv(
+        configs.chapters_json_path,
+        configs.characters_csv,
+        configs.chapters_csv,
+        configs.coc_csv,
+    )
+    scrap_volumes(configs.last_volume, configs.volumes_json_path)
+    scrap_characters(configs.characters_csv, configs.characters_json_path)
 
-    print("fin")
+    print("Finish scrapping")
